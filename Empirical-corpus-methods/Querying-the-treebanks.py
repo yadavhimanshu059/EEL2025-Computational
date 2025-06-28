3ad1481dacfb7e17287ba60d63436d10b9c60ffb
Himanshu Yadav <yadavhimanshu059@gmail.com>
2025-06-28 15:33:43 +0530

    Add files via upload

--- Empirical-corpus-methods/Querying-the-treebanks.py
+++ Empirical-corpus-methods/Querying-the-treebanks.py
@@ -13,4 +13,32 @@ for root, dirs, files in os.walk(directory):
             sud_files.append(fullpath)            # creates a list of path of all files (file of each language) from the directory
 
 print(sud_files)
-#for i in ud_files:                                       # reads file of each language one by one
+
+for i in sud_files:                  # reads file of each language one by one
+    lang = str(i).replace("./SUD", "")
+    lang=lang.replace("-sud-train.conllu", "")            # lang variable stores the language code
+    lang=lang.replace("-sud-test.conllu", "")
+    data_file = open(str(i),'r',encoding='utf-8').read()
+    sentences = []
+    sentences = parse(data_file)                         # parses the CONLLU format
+    print(lang)        
+    for sentence in sentences[0:]:
+        sent_id+=1
+        print(sent_id)
+        if sent_id>1:
+            num_sent += 1
+            tree = nx.DiGraph()                              # An empty directed graph (i.e., edges are uni-directional)
+            for nodeinfo in sentence[0:]:                    # retrieves information of each node from dependency tree in UD format
+                entry=list(nodeinfo.items())
+                if not entry[7][1]=='punct':
+                    tree.add_node(entry[0][1], form=entry[1][1], lemma=entry[2][1], upostag=entry[3][1], xpostag=entry[4][1], feats=entry[5][1], head=entry[6][1], deprel=entry[7][1], deps=entry[8][1], misc=entry[9][1])                #adds node to the directed graph
+            ROOT=0
+            tree.add_node(ROOT)                            # adds an abstract root node to the directed graph
+
+            for nodex in tree.nodes:
+                if not nodex==0:
+                    if tree.has_node(tree.nodes[nodex]['head']):                                         # to handle disjoint trees
+                        tree.add_edge(tree.nodes[nodex]['head'],nodex,drel=tree.nodes[nodex]['deprel'])       # adds edges as relation between nodes
+
+            print(tree.edges)
+
